0aa85e80c381eb97ab671ff92b19c5f10f2f516e
Svelofy <shadoferapps@gmail.com>
2022-11-03 16:02:11 +0200

    Remove `vite` overlay

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			overlay: false
+		}
+	}
 };
 
 export default config;
